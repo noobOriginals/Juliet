2a19db52d3aad0ddccc63eae6395a00c5ef1b329
noobOriginals <noobOriginals@xx.com>
2026-05-17 01:54:47 +0300

    Some CUDA stuff, idk if it works or not

---
+++ src/include/cuda/optics.cuh
@@ -0,0 +1,56 @@
+#ifndef OPTICS_CUH
+#define OPTICS_CUH
+
+// Std includes
+#include <cuda_runtime.h>
+#include <curand_kernel.h>
+
+// Local includes
+#include <util/types.h>
+#include <util/epsilon.h>
+#include <cuda/vec3.cuh>
+#include <cuda/random.cuh>
+
+namespace cuda {
+
+// Returns a random unit vector sampled on the cosine hemisphere around the normal
+inline __device__ vec3 diffuse(curandStatePhilox4_32_10* state, const vec3& normal) {
+    return normalize(randomCosineHemisphere(state, normal));
+}
+
+// Returns a reflected unit vector around the normal. If fuzz > 0.0f, introduces a cosine hemisphere sampled fuzz vector.
+inline __device__ vec3 reflect(curandStatePhilox4_32_10* state, const vec3& v, const vec3& normal, float32 fuzz) {
+    vec3 ref = v - 2.0f * dot(v, normal) * normal;
+    if (fuzz == 0.0f) {
+        return ref;
+    }
+    vec3 fuzzed = ref + randomUV(state) * fuzz;
+    if (lenSq(fuzzed) <= EPSILON * EPSILON) {
+        return ref;
+    }
+    return normalize(fuzzed);
+}
+
+// Schlick (Fresnel) approximation
+inline __device__ float32 reflectance(float32 cos, float32 n1, float32 n2) {
+    float32 r0 = (n1 - n2) / (n1 + n2);
+    r0 *= r0;
+    return r0 + (1.0f - r0) * pow((1.0f - cos), 5);
+}
+
+// Returns the refracted/reflected unit vector (depending on TIR or Schlick)
+inline __device__ vec3 refract(curandStatePhilox4_32_10* state, const vec3& dir, const vec3& normal, float32 n1, float32 n2) {
+    float32 cos = dot(-dir, normal);
+    float32 sin = sqrt(max(0.0f, 1.0f - cos * cos));
+    float32 idx = n1 / n2;
+    if (sin * idx > 1.0f || reflectance(cos, n1, n2) > nextGaussian(state)) {
+        return dir + 2.0f * cos * normal;
+    }
+    vec3 perp = idx * (dir + cos * normal);
+    vec3 para = -sqrt(fabs(1.0f - dot(perp, perp))) * normal;
+    return perp + para;
+}
+
+} // namespace cuda
+
+#endif // OPTICS_CUH
---
+++ src/include/cuda/random.cuh
@@ -0,0 +1,71 @@
+#ifndef RANDOM_CUH
+#define RANDOM_CUH
+
+// Std includes
+#include <cuda_runtime.h>
+#include <curand_kernel.h>
+
+// Local includes
+#include <util/types.h>
+#include <util/epsilon.h>
+#include <cuda/vec3.cuh>
+
+namespace cuda {
+
+inline __device__ void initState(curandStatePhilox4_32_10* state, uint64 seed) {
+    curand_init(seed, 12345, 0, state);
+}
+
+inline __global__ void initStatesKernel1D(curandStatePhilox4_32_10* states, uint64 numStates) {
+    uint64 idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < numStates) {
+        initState(&states[idx], idx);
+    }
+}
+
+inline __global__ void initStatesKernel2D(curandStatePhilox4_32_10* states, uint64 numStates) {
+    uint64 idx = (blockIdx.y * blockDim.y + threadIdx.y) * gridDim.x * blockDim.x +  blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < numStates) {
+        initState(&states[idx], idx);
+    }
+}
+
+inline __global__ void initStatesKernel3D(curandStatePhilox4_32_10* states, uint64 numStates) {
+    uint64 idx = (blockIdx.z * blockDim.z + threadIdx.z) * gridDim.x * blockDim.x * gridDim.y * blockDim.y + (blockIdx.y * blockDim.y + threadIdx.y) * gridDim.x * blockDim.x +  blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < numStates) {
+        initState(&states[idx], idx);
+    }
+}
+
+inline __device__ float32 nextFloat(curandStatePhilox4_32_10* state) {
+    return curand_uniform(state);
+}
+
+inline __device__ float32 nextGaussian(curandStatePhilox4_32_10* state) {
+    return curand_normal(state);
+}
+
+inline __device__ vec3 randomVec3(curandStatePhilox4_32_10* state) {
+    return vec3(nextFloat(state), nextFloat(state), nextFloat(state));
+}
+
+inline __device__ vec3 randomUV(curandStatePhilox4_32_10* state) {
+    return vec3(nextGaussian(state), nextGaussian(state), nextGaussian(state));
+}
+
+inline __device__ vec3 randomHemisphere(curandStatePhilox4_32_10* state, const vec3& normal) {
+    vec3 v = randomUV(state);
+    return (dot(normal, v) < 0.0f) ? -v : v;
+}
+
+inline __device__ vec3 randomCosineHemisphere(curandStatePhilox4_32_10* state, const vec3& normal) {
+    vec3 v = normal + randomUV(state);
+    if (lenSq(v) <= EPSILON * EPSILON) {
+        return normal;
+    }
+    return v;
+}
+
+} // namespace cuda
+
+#endif // RANDOM_CUH
